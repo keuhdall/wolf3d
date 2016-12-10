13cc5c2f10e86f0e633af463c75cbdaf6a0dddff
keuhdall <lmarques@student.42.fr>
2016-12-10 14:49:04 +0100

    started implementing structs

--- wolf3d.h
+++ wolf3d.h
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/25 01:19:41 by lmarques          #+#    #+#             */
-/*   Updated: 2016/12/07 16:21:12 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/12/10 14:48:26 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@
 # include "./libft/libft.h"
 # include "./libft/get_next_line.h"
 
+typedef struct	s_point
+{
+	int			x;
+	int			y;
+	int			id;
+}				t_point;
+
 typedef struct	s_meta
 {
 	void		*ptr;
@@ -24,12 +31,35 @@ typedef struct	s_meta
 	void		*img;
 }				t_meta;
 
-typedef struct	s_point
+typedef struct	s_camera
 {
-	int			x;
-	int			y;
-	int			id;
-}				t_point;
+	double		pos_x;
+	double		screen_x;
+	double		screen_y;
+}				t_camera;
+
+typedef struct	s_player
+{
+	double		pos_x;
+	double		pos_y;
+	int			map_pos_x;
+	int			map_pos_y;
+	char		collide;
+	char		collide_side;
+	double		ray_x; // rayPosX
+	double		ray_y; // rayPosY
+	double		look_x; // DirX
+	double		look_y; // DirY
+	int			look_x_sign;
+	int			look_y_sign;
+	double		dist_side_x; // Length between the player and the first edge of a map square (x axis)
+	double		dist_side_y; // Same but for y axis
+	double		diff_side_x; // Next length between 2 x axis
+	double		diff_side_y; // Next length between 2 y axis
+	double		ray_len; // Length of a ray (between player and a wall)
+	int			line_start; // Start point of line (y axis)
+	int			line_end; // End point of line (y axis)
+}				t_player;
 
 typedef struct	s_wrapper
 {
