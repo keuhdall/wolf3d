42f4b994f536a39820283b09c78ca89ecf7c9d2b
Léonard Marques <lmarques@student.42.fr>
2016-12-12 17:26:17 +0100

    renamed ft_tools.c in tools.c; started working on the raycasting algorithm

--- tools.c
+++ tools.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_tools.c                                         :+:      :+:    :+:   */
+/*   tools.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/11 21:37:15 by lmarques          #+#    #+#             */
-/*   Updated: 2016/12/11 23:26:12 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/12/12 17:12:43 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,9 +46,41 @@ t_dpoint	ft_search_id(t_point *tab, int len, int id)
 
 void		ft_init_struct(t_player *p, t_point *tab, int tab_len)
 {
+	p->mlx.ptr = mlx_init();
+	p->mlx.win = mlx_new_window(p->mlx.ptr, 800, 600, "wolf3d");
+	p->mlx.img = mlx_new_image(p->mlx.ptr, 800, 600);
+	p->mlx.data = (int *)mlx_get_data_addr(p->mlx.img, &p->mlx.bpp,
+		&p->mlx.size_line, &p->mlx.endian);
 	p->pos = ft_search_id(tab, tab_len, -1);
-	p->look.x = -1;
-	p->look.y = 0;
-	p->c.screen_x = 0;
-	p->c.screen_x = 0.66;
+	p->p_dir.x = -1;
+	p->p_dir.y = 0;
+	p->c.screen.x = 0;
+	p->c.screen.y = 0.66;
+	p->collide = 'n';
+	p->collide_side = 'n';
+}
+
+void		ft_reset_values(t_player *p, int count)
+{
+	p->c.pos_x = 2 * count / W_WIDTH - 1;
+	p->ray_pos = p->pos;
+	p->ray_dir.x = p->p_dir.x + p->c.screen.x * p->c.pos_x;
+	p->ray_dir.y = p->p_dir.y + p->c.screen.y * p->c.pos_x;
+	p->map_pos.x = (int)p->ray_pos.x;
+	p->map_pos.y = (int)p->ray_pos.y;
+	p->diff_side.x = sqrt(1 + pow(p->ray_dir.y, 2) / pow(p->ray_dir.x, 2));
+	p->diff_side.y = sqrt(1 + pow(p->ray_dir.x, 2) / pow(p->ray_dir.y, 2));
+	p->collide = 'n';
+}
+
+void		ft_draw(t_player *p)
+{
+	int	count;
+
+	count = 0;
+	while (count < W_WIDTH)
+	{
+		ft_reset_values(p, count);
+		count++;
+	}
 }
