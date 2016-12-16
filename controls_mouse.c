aed4eb6202777ac164f96c2fe04617978bce82cf
Léonard Marques <lmarques@student.42.fr>
2016-12-16 04:02:44 +0100

    created controls_mouse.c

---
+++ controls_mouse.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   controls_mouse.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/16 03:54:38 by lmarques          #+#    #+#             */
+/*   Updated: 2016/12/16 03:58:25 by lmarques         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "wolf3d.h"
+
+int	ft_rotate(int x, int y, t_player *p)
+{
+	static int	old_x = 400;
+	double		old_dir_x;
+	double		old_screen_x;
+
+	old_dir_x = p->p_dir.x;
+	old_screen_x = p->c.screen.x;
+	if (x > old_x)
+	{
+		p->p_dir.x = p->p_dir.x * cos(-0.1) - p->p_dir.y * sin(-0.1);
+		p->p_dir.y = old_dir_x * sin(-0.1) + p->p_dir.y * cos(-0.1);
+		p->c.screen.x = p->c.screen.x * cos(-0.1) - p->c.screen.y * sin(-0.1);
+		p->c.screen.y = old_screen_x * sin(-0.1) + p->c.screen.y * cos(-0.1);
+	}
+	else
+	{
+		p->p_dir.x = p->p_dir.x * cos(0.1) - p->p_dir.y * sin(0.1);
+		p->p_dir.y = old_dir_x * sin(0.1) + p->p_dir.y * cos(0.1);
+		p->c.screen.x = p->c.screen.x * cos(0.1) - p->c.screen.y * sin(0.1);
+		p->c.screen.y = old_screen_x * sin(0.1) + p->c.screen.y * cos(0.1);
+	}
+	old_x = x + (y - y);
+	mlx_clear_window(p->mlx.ptr, p->mlx.win);
+	ft_clear_image(p);
+	ft_draw(p);
+	mlx_put_image_to_window(p->mlx.ptr, p->mlx.win, p->mlx.img, 0, 0);
+	return (0);
+}
