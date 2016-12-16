bde3a466994d38c03a197185ae639d5de30f6161
keuhdall <lmarques@student.42.fr>
2016-12-16 18:31:44 +0100

    checked norm

--- controls_mouse.c
+++ controls_mouse.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/16 03:54:38 by lmarques          #+#    #+#             */
-/*   Updated: 2016/12/16 03:58:25 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/12/16 18:30:14 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,14 @@ int	ft_rotate(int x, int y, t_player *p)
 
 	old_dir_x = p->p_dir.x;
 	old_screen_x = p->c.screen.x;
-	if (x > old_x)
-	{
-		p->p_dir.x = p->p_dir.x * cos(-0.1) - p->p_dir.y * sin(-0.1);
-		p->p_dir.y = old_dir_x * sin(-0.1) + p->p_dir.y * cos(-0.1);
-		p->c.screen.x = p->c.screen.x * cos(-0.1) - p->c.screen.y * sin(-0.1);
-		p->c.screen.y = old_screen_x * sin(-0.1) + p->c.screen.y * cos(-0.1);
-	}
-	else
-	{
-		p->p_dir.x = p->p_dir.x * cos(0.1) - p->p_dir.y * sin(0.1);
-		p->p_dir.y = old_dir_x * sin(0.1) + p->p_dir.y * cos(0.1);
-		p->c.screen.x = p->c.screen.x * cos(0.1) - p->c.screen.y * sin(0.1);
-		p->c.screen.y = old_screen_x * sin(0.1) + p->c.screen.y * cos(0.1);
-	}
+	p->p_dir.x =  x > old_x ? p->p_dir.x * cos(-0.1) - p->p_dir.y *
+	sin(-0.1) : p->p_dir.x * cos(0.1) - p->p_dir.y * sin(0.1);
+	p->p_dir.y = x > old_x ?  old_dir_x * sin(-0.1) + p->p_dir.y *
+	cos(-0.1) : old_dir_x * sin(0.1) + p->p_dir.y * cos(0.1);
+	p->c.screen.x = x > old_x ? p->c.screen.x * cos(-0.1) - p->c.screen.y *
+	sin(-0.1) : p->c.screen.x * cos(0.1) - p->c.screen.y * sin(0.1);
+	p->c.screen.y = x > old_x ? old_screen_x * sin(-0.1) + p->c.screen.y *
+	cos(-0.1) : old_screen_x * sin(0.1) + p->c.screen.y * cos(0.1);
 	old_x = x + (y - y);
 	mlx_clear_window(p->mlx.ptr, p->mlx.win);
 	ft_clear_image(p);
