6f9ab169d18c80d31f3327f61955611581909e2d
Léonard Marques <lmarques@student.42.fr>
2016-12-16 03:12:06 +0100

    added strafe controls; controls now works with WASD

--- controls_keyboard.c
+++ controls_keyboard.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/14 19:36:05 by lmarques          #+#    #+#             */
-/*   Updated: 2016/12/15 18:29:24 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/12/16 03:11:04 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void	ft_rotate(int keycode, t_player *p)
 
 void	ft_move(int keycode, t_player *p)
 {
-	if (keycode == 126)
+	if (keycode == 13)
 	{
 		if (p->tab[(int)p->pos.y * ft_get_len(p->tab) +
 			((int)p->pos.x + (int)p->p_dir.x)].id == 0)
@@ -50,7 +50,7 @@ void	ft_move(int keycode, t_player *p)
 			ft_get_len(p->tab) + (int)p->pos.x].id == 0)
 			p->pos.y += p->p_dir.y;
 	}
-	if (keycode == 125)
+	if (keycode == 1)
 	{
 		if (p->tab[(int)p->pos.y * ft_get_len(p->tab) +
 			((int)p->pos.x + (int)p->p_dir.x)].id == 0)
@@ -65,13 +65,41 @@ void	ft_move(int keycode, t_player *p)
 	mlx_put_image_to_window(p->mlx.ptr, p->mlx.win, p->mlx.img, 0, 0);
 }
 
+void	ft_strafe(int keycode, t_player *p)
+{
+	if (keycode == 0)
+	{
+		if (p->tab[((int)p->pos.y + (int)p->p_dir.x) *
+			ft_get_len(p->tab) + (int)p->pos.x].id == 0)
+		{
+			p->pos.x -= p->p_dir.y;
+			p->pos.y += p->p_dir.x;
+		}
+	}
+	if (keycode == 2)
+	{
+		if (p->tab[((int)p->pos.y + (int)p->p_dir.x) *
+			ft_get_len(p->tab) + (int)p->pos.x].id == 0)
+		{
+			p->pos.x += p->p_dir.y;
+			p->pos.y -= p->p_dir.x;
+		}
+	}
+	mlx_clear_window(p->mlx.ptr, p->mlx.win);
+	ft_clear_image(p);
+	ft_draw(p);
+	mlx_put_image_to_window(p->mlx.ptr, p->mlx.win, p->mlx.img, 0, 0);
+}
+
 int		ft_manage_keyboard(int keycode, t_player *p)
 {
 	if (keycode == 53)
 		exit(1);
 	if (keycode == 123 || keycode == 124)
 		ft_rotate(keycode, p);
-	if (keycode == 125 || keycode == 126)
+	if (keycode == 13 || keycode == 1)
 		ft_move(keycode, p);
+	if (keycode == 2 || keycode == 0)
+		ft_strafe(keycode, p);
 	return (0);
 }
