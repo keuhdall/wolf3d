66f8d4c633237b6cd23e2d439ed35b2ec410e5db
keuhdall <lmarques@student.42.fr>
2017-01-03 18:32:59 +0100

    checked norm in floor.c

--- floor.c
+++ floor.c
@@ -6,13 +6,13 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/22 20:38:56 by lmarques          #+#    #+#             */
-/*   Updated: 2017/01/03 16:26:33 by lmarques         ###   ########.fr       */
+/*   Updated: 2017/01/03 18:31:38 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
-void	ft_set_floor(t_player *p)
+void		ft_set_floor(t_player *p)
 {
 	if (p->found_wall_side == 'n' && p->ray_dir.x > 0)
 	{
@@ -36,47 +36,42 @@ void	ft_set_floor(t_player *p)
 	}
 }
 
-void	ft_set_floor_to_buffer(t_player *p, int count, t_point pt)
+void		ft_set_textures(t_player *p, t_point *tex_floor,
+			t_point *tex_ceiling, t_dpoint *floor)
 {
-	ft_set_floor(p);
+	tex_floor->x = ((int)(floor->x * p->mlx.texture[0].width)) %
+	p->mlx.texture[0].width;
+	tex_floor->y = ((int)(floor->y * p->mlx.texture[0].height)) %
+	p->mlx.texture[0].height;
+	tex_ceiling->x = (int)(floor->x * p->mlx.texture[1].width) %
+	p->mlx.texture[1].width;
+	tex_ceiling->y = (int)(floor->y * p->mlx.texture[1].height) %
+	p->mlx.texture[1].height;
+}
+
+void		ft_set_floor_to_buffer(t_player *p, int count, t_point pt)
+{
 	int			*tmp;
-	double		distWall, distPlayer, currentDist;
+	double		weight;
 	t_point		tex_floor;
 	t_point		tex_ceiling;
 	t_dpoint	floor;
 
-	distWall = p->ray_len;
-	distPlayer = 0.0;
 	pt.y = pt.y < 0 ? W_HEIGHT : pt.y;
+	ft_set_floor(p);
 	while (++pt.y < W_HEIGHT)
 	{
-		currentDist = W_HEIGHT / (2.0 * pt.y - W_HEIGHT);
-		double weight = (currentDist - distPlayer) / (distWall - distPlayer);
-		floor.x = weight * p->floor_wall.x + (1.0 - weight) *
-		p->pos.x;
-		floor.y = weight * p->floor_wall.y + (1.0 - weight) *
-		p->pos.y;
-		tex_floor.x = ((int)(floor.x * p->mlx.texture[0].width)) %
-		p->mlx.texture[0].width;
-		tex_floor.y = ((int)(floor.y * p->mlx.texture[0].height)) %
-		p->mlx.texture[0].height;
-
-		tex_ceiling.x = (int)(floor.x * p->mlx.texture[1].width) %
-		p->mlx.texture[1].width;
-		tex_ceiling.y = (int)(floor.y * p->mlx.texture[1].height) %
-		p->mlx.texture[1].height;
-
-
+		weight = (W_HEIGHT / (2.0 * pt.y - W_HEIGHT)) / p->ray_len;
+		floor.x = weight * p->floor_wall.x + (1.0 - weight) * p->pos.x;
+		floor.y = weight * p->floor_wall.y + (1.0 - weight) * p->pos.y;
+		ft_set_textures(p, &tex_floor, &tex_ceiling, &floor);
 		tmp = (int *)mlx_get_data_addr(p->mlx.texture[0].tex,
 		&p->mlx.bpp, &p->mlx.size_line, &p->mlx.endian);
-
 		p->mlx.data[pt.y * W_WIDTH + count] =
 		(tmp[tex_floor.y * p->mlx.texture[0].width + tex_floor.x] >> 1)
 		& 8355711;
-
 		tmp = (int *)mlx_get_data_addr(p->mlx.texture[1].tex,
 		&p->mlx.bpp, &p->mlx.size_line, &p->mlx.endian);
-
 		p->mlx.data[(W_HEIGHT - pt.y) * W_WIDTH + count] =
 		tmp[p->mlx.texture[1].width * tex_ceiling.y + tex_ceiling.x];
 	}
